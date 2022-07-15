0f81c406830ffb1ac62edb406b2ccfb92bd5c0e2
Taylor Savage <taylorsavage93@gmail.com>
2022-07-15 14:58:10 -0400

    finished exercise 1

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using StaticExercise;
+
+var celcius = TempConverter.FarenheitToCelcius(68);
+var farenheit = TempConverter.CelciusToFarenheit(10);
+
+Console.WriteLine($"68 degrees farenheit is the same as {celcius} degrees celcius");
+Console.WriteLine($"10 degrees  celcius is the same as {farenheit} degrees farenheit");
+
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempConverter
+    {
+        public static double FarenheitToCelcius(double farenheit)
+        {
+          var result = (farenheit - 32) / 1.8;
+
+          return result;  
+            
+        }
+        public static double CelciusToFarenheit(double celcius)
+        {
+            var result = (celcius * 9) / 5 + 32;
+
+            return result;
+        }
+    }
+}
